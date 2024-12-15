926dc1825c6360eb0abd2bbdc479ebdcc7b5eb0e
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2024-12-15 17:58:33 +0300

    Addition of typescript to the vue instance

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,17 +4,22 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+// @ts-ignore
+import {ZiggyVue} from '../../vendor/tightenco/ziggy';
+import type {DefineComponent} from "vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(
+        `./Pages/${name}.vue`,
+         import.meta.glob<DefineComponent>("./Pages/**/*.vue")
+    ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, (window as any).Ziggy)
             .mount(el);
     },
     progress: {
